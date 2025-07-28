9e8316105ba76d1b3ca98f2b6c85fd71d9073338
ConnerDeFeo <ninjanerozz@gmail.com>
2025-07-28 15:54:53 -0400

    tests working with sam framework

---
+++ Server/test/athlete/test_athlete.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import boto3
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
+from Server.lambdas.athlete.create_athlete.create_athlete import create_athlete
+
+def test_create_athlete():
+    event = {
+        "first_name": "John",
+        "last_name": "Doe",
+        "email": "john.doe@example.com",
+        "username": "john.doe",
+        "password": "password"
+    }
+    response = create_athlete(event, {})
+    assert response['statusCode'] == 200
+
+    # Test that the athlete was created
+    dynamodb = boto3.resource('dynamodb', endpoint_url='http://localhost:8000')
+    table = dynamodb.Table('athletes')
+    response = table.get_item(Key={'username': 'john.doe'})
+    assert response['Item'] == event

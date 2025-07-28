b7cd5b482446af60e05696e8cd0326994ed52e36
ConnerDeFeo <ninjanerozz@gmail.com>
2025-07-28 12:53:47 -0400

    gonna use terraform instead, using docker + pytest for testing lambdas

--- Server/lambdas/athletes/create_athlete/create_athlete.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def create_athlete(event, context):
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "athlete created",
-        }),
-    }

9840721bfe5158d04bf5967f1f10362b4d363718
ConnerDeFeo <ninjanerozz@gmail.com>
2025-07-28 16:36:00 -0400

    not sure why this is doing this

--- Server/test/athlete/test_athlete.py
+++ Server/test/athlete/test_athlete.py
@@ -1,17 +1,12 @@
 import sys
 import os
 import boto3
+import json
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
 from Server.lambdas.athlete.create_athlete.create_athlete import create_athlete
 
 def test_create_athlete():
-    event = {
-        "first_name": "John",
-        "last_name": "Doe",
-        "email": "john.doe@example.com",
-        "username": "john.doe",
-        "password": "password"
-    }
+    event = json.load(open('events/create_athlete.json'))
     response = create_athlete(event, {})
     assert response['statusCode'] == 200
 

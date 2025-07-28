7b990a4e5c37544026ecb7a5d57215dbda29ad6e
ConnerDeFeo <ninjanerozz@gmail.com>
2025-07-28 18:45:18 -0400

    not sure what happened but its working now?:

--- Server/lambdas/athlete/create_athlete/create_athlete.py
+++ Server/lambdas/athlete/create_athlete/create_athlete.py
@@ -1,30 +1,30 @@
 import boto3
-# import os
-# from dotenv import load_dotenv
-# load_dotenv()
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def create_athlete(event, context):
-    # #Connect to DynamoDB
-    # dynamodb = boto3.resource(
-    #     "dynamodb",
-    #     endpoint_url=os.getenv("DYNAMODB_ENDPOINT_URL"),
-    #     aws_access_key_id="dummy",
-    #     aws_secret_access_key="dummy",
-    #     region_name="us-east-2"
-    # )
-    # table = dynamodb.Table('athletes')
+    #Connect to DynamoDB
+    dynamodb = boto3.resource(
+        "dynamodb",
+        endpoint_url=os.getenv("DYNAMODB_ENDPOINT_URL"),
+        aws_access_key_id="dummy",
+        aws_secret_access_key="dummy",
+        region_name="us-east-2"
+    )
+    table = dynamodb.Table('athletes')
 
-    # #Create athlete
-    # table.put_item(
-    #     Item={
-    #         'first_name': event['first_name'],
-    #         'last_name': event['last_name'],
-    #         'email': event['email'],
-    #         'password': event['password'],
-    #         'username': event['username']
-    #     }
-    # )
+    #Create athlete
+    table.put_item(
+        Item={
+            'first_name': event['first_name'],
+            'last_name': event['last_name'],
+            'email': event['email'],
+            'password': event['password'],
+            'username': event['username']
+        }
+    )
     return {
         "statusCode": 200
     }

df75b8c5ac6beb4065fac9916e0c9680c2de910a
ConnerDeFeo <ninjanerozz@gmail.com>
2025-07-30 12:26:25 -0400

    alright time to watch some intro to this shit

--- Client/App.tsx
+++ Client/App.tsx
@@ -1,22 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import TestComponent from './pages/testing';
 import './global.css'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <TestComponent/>
+    <View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

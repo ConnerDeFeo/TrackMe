d8531ebedd097a84b0107b745f8433a440f5ec53
ConnerDeFeo <ninjanerozz@gmail.com>
2025-07-31 12:50:12 -0400

    android emulator working full stack

--- Client/services/AthleteService.ts
+++ Client/services/AthleteService.ts
@@ -1,9 +1,11 @@
 import API from "./API";
+const EXPO_PUBLIC_API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 const AthleteService = {
     //Creates athlete
     createAthlete: async (athleteData:Record<string, string>) => {
-        const response = await API.post('http://localhost:3000/athletes/create_athlete', athleteData);
+        console.log(EXPO_PUBLIC_API_URL);
+        const response = await API.post(`${EXPO_PUBLIC_API_URL}/athletes/create_athlete`, athleteData);
         if (!response.ok) {
             throw new Error('Failed to create athlete');
         }

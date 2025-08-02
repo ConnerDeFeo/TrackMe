e6b39e94d7a69e4d7873c3b5cafb74bec5929eac
ConnerDeFeo <ninjanerozz@gmail.com>
2025-08-02 05:04:42 -0400

    trying to figure out the excersise relations

--- Server/dev-setup/setup.sql
+++ Server/dev-setup/setup.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS groups CASCADE;
 DROP TABLE IF EXISTS athlete_groups CASCADE;
 DROP TABLE IF EXISTS athlete_group_invites CASCADE;
 
+--User and user relation related tables--
 CREATE TABLE coaches (
     userId VARCHAR(255) PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE
@@ -33,4 +34,47 @@ CREATE TABLE athlete_group_invites (
     athleteId VARCHAR(255) REFERENCES athletes(userId),
     groupId INT REFERENCES groups(id),
     UNIQUE (athleteId, groupId)
+);
+
+--Workout related tables--
+
+CREATE TABLE workout_inputs(
+    id SERIAL PRIMARY KEY,
+    athleteId VARCHAR(255) REFERENCES athletes(userId),
+    distance FLOAT NOT NULL,
+    time FLOAT NOT NULL,
+    date VARCHAR(10) DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE workouts(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT DEFAULT '',
+);
+
+CREATE TABLE group_workouts(
+    groupId INT REFERENCES groups(id),
+    workoutId INT REFERENCES workouts(id),
+    PRIMARY KEY (groupId, workoutId),
+    date VARCHAR(10) DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE excersies (
+    id SERIAL PRIMARY KEY,
+    notes VARCHAR(255) NOT NULL,
+    description TEXT DEFAULT '',
+    sets INT DEFAULT 1 NOT NULL,
+    reps INT DEFAULT 1 NOT NULL
+);
+
+CREATE TABLE excersies_parts(
+    id SERIAL PRIMARY KEY,
+    distance FLOAT NOT NULL,
+    measurement VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE workout_excersies(
+    workoutId INT REFERENCES workouts(id),
+    excersiesId INT REFERENCES excersies(id),
+    PRIMARY KEY (workoutId, excersiesId)
 );

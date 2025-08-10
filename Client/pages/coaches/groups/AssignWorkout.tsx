8cb6f1b53e3013b767ac7204261a9a2da4b63094
ConnerDeFeo <ninjanerozz@gmail.com>
2025-08-10 13:21:18 -0400

    working on assigning a workout

---
+++ Client/pages/coaches/groups/AssignWorkout.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const AssignWorkout = ()=>{
+    return (
+        <View>
+            <Text>Assign Workout</Text>
+        </View>
+    );
+}
+
+export default AssignWorkout;

74dbf35dd5396702cf87128ef54b71ee8bc05744
ConnerDeFeo <ninjanerozz@gmail.com>
2025-08-18 15:52:59 -0400

    footer complete for now

--- Client/components/Footer.tsx
+++ Client/components/Footer.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import { View, TouchableOpacity, Text } from 'react-native';
+import { View, TouchableOpacity, Text, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+
+// Import all images statically
+const images = {
+    'Groups': require('../images/Groups.png'),
+    'Inputs': require('../images/Groups.png'),
+    'Coaches': require('../images/Groups.png'),
+    'Workouts': require('../images/Workouts.png'),
+    'Athletes': require('../images/Athletes.png'),
+};
 
 //Footer at bottom of the screen with navigation buttons
 const Footer: React.FC<{ buttons: string[][] }> = ({ buttons }) => {
     const navigation = useNavigation<any>();
 
     return (
-        <View className='h-[5rem] bg-blue-500 flex-row justify-around items-center'>
-            {buttons.map(([label, destination], idx) => (
+        <View className='h-[5rem] border-t flex-row justify-around items-center'>
+            {buttons.map(([imageName, destination], idx) => (
                 <TouchableOpacity
                     key={idx}
                     onPress={() => navigation.navigate(destination)}
                 >
-                    <Text className='text-white text-xl'>{label}</Text>
+                    <Image source={images[imageName as keyof typeof images]} className="h-12 w-12" />
                 </TouchableOpacity>
             ))}
         </View>

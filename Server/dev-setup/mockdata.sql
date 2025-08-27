330a0ea9566ff5d7d237d4414363617c4d20be96
ConnerDeFeo <ninjanerozz@gmail.com>
2025-08-26 21:42:38 -0400

    fixed setup script

--- Server/dev-setup/mockdata.sql
+++ Server/dev-setup/mockdata.sql
@@ -3,9 +3,12 @@ Insert into athletes (userId, username) values ('912b25e0-c091-700b-a580-dbad51f
 Insert into athletes (userId, username) values ('51cb2510-30c1-7063-474c-bbc37fc07bcc', 'testdefeo');
 Insert into athlete_coaches (coachId, athleteId) values('81cbd5d0-c0a1-709a-560f-ceb88b7d53d9','912b25e0-c091-700b-a580-dbad51f124e6');
 Insert into athlete_coaches (coachId, athleteId) values('81cbd5d0-c0a1-709a-560f-ceb88b7d53d9','51cb2510-30c1-7063-474c-bbc37fc07bcc');
-Insert into groups (coachId, name) values ('81cbd5d0-c0a1-709a-560f-ceb88b7d53d9', 'Coach DeFeos group');
+Insert into groups (coachId, name) values ('81cbd5d0-c0a1-709a-560f-ceb88b7d53d9', 'Domain Expansion');
+Insert into groups (coachId, name) values ('81cbd5d0-c0a1-709a-560f-ceb88b7d53d9', 'Infinite Void');
 Insert into athlete_groups (athleteId, groupId) values ('912b25e0-c091-700b-a580-dbad51f124e6', 1);
 Insert into athlete_groups (athleteId, groupId) values ('51cb2510-30c1-7063-474c-bbc37fc07bcc', 1);
+Insert into athlete_groups (athleteId, groupId) values ('912b25e0-c091-700b-a580-dbad51f124e6', 2);
+Insert into athlete_groups (athleteId, groupId) values ('51cb2510-30c1-7063-474c-bbc37fc07bcc', 2);
 INSERT INTO workouts (coachId, title, description, exercises) 
 VALUES (
     '81cbd5d0-c0a1-709a-560f-ceb88b7d53d9',
@@ -45,3 +48,4 @@ VALUES (
     ]'::jsonb
 );
 INSERT INTO group_workouts (groupId, workoutId) VALUES (1, 1);
+INSERT INTO group_workouts (groupId, workoutId) VALUES (2, 1);

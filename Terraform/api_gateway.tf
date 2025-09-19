e69ebb20694c8a19c51af36ba02f2c11ee08a7b2
ConnerDeFeo <ninjanerozz@gmail.com>
2025-09-19 15:40:38 -0400

    think i deployed wrong whoops

--- Terraform/api_gateway.tf
+++ Terraform/api_gateway.tf
@@ -39,10 +39,10 @@ locals {
 
     # DELETE Lambdas
     "coaches/remove_group_athlete" = { lambda = aws_lambda_function.lambdas["remove_group_athlete"], method = "DELETE" }
-    "coaches/delete_workout_template" = { lambda = aws_lambda_function.lambdas["delete_workout"], method = "DELETE" }
+    "coaches/delete_workout_template" = { lambda = aws_lambda_function.lambdas["delete_workout_template"], method = "DELETE" }
     "general/remove_coach_athlete" = { lambda = aws_lambda_function.lambdas["remove_coach_athlete"], method = "DELETE" }
     "coaches/delete_group" = { lambda = aws_lambda_function.lambdas["delete_group"], method = "DELETE" }
-    "coaches/delete_group_workouts" = { lambda = aws_lambda_function.lambdas["delete_group_workouts"], method = "DELETE" }
+    "coaches/delete_group_workout" = { lambda = aws_lambda_function.lambdas["delete_group_workout"], method = "DELETE" }
     "coaches/decline_athlete_request" = { lambda = aws_lambda_function.lambdas["decline_athlete_request"], method = "DELETE" }
     "athletes/decline_coach_invite" = { lambda = aws_lambda_function.lambdas["decline_coach_invite"], method = "DELETE" }
   }

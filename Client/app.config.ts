b3a88742de07ad73b5a5fe8d9fa83850160e4972
ConnerDeFeo <ninjanerozz@gmail.com>
2025-09-19 21:56:24 -0400

    trying to figure ts out wth

---
+++ Client/app.config.ts
@@ -0,0 +1,5 @@
+import {ConfigContext, ExpoConfig} from 'expo/config';
+
+const EAS_PROJECT_ID = '7a30db18-1202-43a9-9ca5-8c93acc236ed';
+const EAS_PROJECT_OWNER = 'connerjackdefeo';
+const EAS_ORG_SLUG = 'Client';

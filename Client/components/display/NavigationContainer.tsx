e930fcc6dd6a20e5cc0238a25cc4f7c97b9233f9
ConnerDeFeo <ninjanerozz@gmail.com>
2025-10-02 21:46:14 -0400

    nvm i have to use migrations sigh

--- Client/components/display/NavigationContainer.tsx
+++ Client/components/display/NavigationContainer.tsx
@@ -6,7 +6,7 @@ const NavigationContainer = ({navigateTo, text}: {navigateTo: () => void, text: 
         <Pressable 
             onPress={navigateTo}
         >
-            <View className="bg-white my-1 p-6 rounded-lg shadow-sm border-b-2 trackme-border-gray flex-row items-center justify-between">
+            <View className="bg-white my-1 p-4 rounded-lg shadow-sm border-b-2 trackme-border-gray flex-row items-center justify-between">
                 <Text className="text-gray-800 text-xl font-bold">
                     {text}
                 </Text>

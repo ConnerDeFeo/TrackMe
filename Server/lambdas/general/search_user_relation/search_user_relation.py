26144cdc282a609e00d4b66c2906e5bcc90de875
ConnerDeFeo <ninjanerozz@gmail.com>
2025-10-03 22:52:58 -0400

    search users funciton

---
+++ Server/lambdas/general/search_user_relation/search_user_relation.py
@@ -0,0 +1,44 @@
+import json
+from user_auth import get_user_info
+from rds import fetch_all
+
+def search_user_relation(event, context):
+    query_params = event.get('queryStringParameters', {})
+
+    try:
+        user_info = get_user_info(event)
+        user_id = user_info['userId']
+        search_term = query_params.get('searchTerm', '')
+
+        # Search user_relations, if no relation from user --> other, not added
+        # If user --> other one way, pending
+        # if other --> user one way, awaiting acceptance
+        # if both ways, added
+        search_query = """
+            SELECT u.userId, u.username, u.firstName, u.lastName, u.accountType,
+                CASE
+                    WHEN ur1.userId IS NOT NULL AND ur2.userId IS NOT NULL THEN 'added'
+                    WHEN ur1.userId IS NOT NULL AND ur2.userId IS NULL THEN 'pending'
+                    WHEN ur1.userId IS NULL AND ur2.userId IS NOT NULL THEN 'awaiting response'
+                    ELSE 'not added'
+                END AS relation_status
+            FROM users u
+            LEFT JOIN user_relations ur1 
+                ON ur1.relationId = u.userId
+                AND ur1.userId = %s
+            LEFT JOIN user_relations ur2
+                ON u.userId = ur2.userId
+                AND ur2.relationId = %s
+            WHERE u.userId != %s
+        """
+        fetch_all(
+            search_query,
+            (user_id, user_id, user_id)
+        )
+
+    except Exception as e:
+        print(f"Error retrieving user info: {e}")
+        return {
+            'statusCode': 401,
+            'body': json.dumps({'error': 'Unauthorized', 'message': str(e)})
+        }

ef2152f677a3af82184ef217d9cd4f3793e084d0
ConnerDeFeo <ninjanerozz@gmail.com>
2025-10-05 15:23:42 -0400

    WE DID IT BABY LETS GO

--- Server/lambdas/general/get_groups/get_groups.py
+++ Server/lambdas/general/get_groups/get_groups.py
@@ -1,6 +1,6 @@
 import json
 from rds import fetch_all
-from user_auth import get_user_info
+from user_auth import get_user_info, get_auth_header
 
 #Gets all related groups for an athlete or coach
 def get_groups(event, context):
@@ -9,6 +9,7 @@ def get_groups(event, context):
         user_info = get_user_info(event)
         userId = user_info["userId"]
         accountType = user_info['accountType']
+        auth_header = get_auth_header()
 
         #Athletes
         if accountType == 'Athlete':
@@ -38,16 +39,19 @@ def get_groups(event, context):
         if group_data:
             return {
                 "statusCode": 200,
-                "body": json.dumps(group_data)
+                "body": json.dumps(group_data),
+                "headers": auth_header
             }
         else:
             return {
                 "statusCode": 404,
-                "body": json.dumps({"error": "Group not found"})
+                "body": json.dumps({"error": "Group not found"}),
+                "headers": auth_header
             }
     except Exception as e:
         print(f"Error retrieving group: {e}")
         return {
             "statusCode": 500,
-            "body": json.dumps({"error": str(e)})
+            "body": json.dumps({"error": str(e)}),
+            "headers": auth_header
         }
--- Server/layers/user_auth/python/user_auth.py
+++ Server/layers/user_auth/python/user_auth.py
@@ -44,3 +44,31 @@ def get_user_info(event):
         }
     except Exception as e:
         raise Exception(f"Unauthorized: {str(e)}")
+    
+def get_auth_header():
+    return  {
+        "Access-Control-Allow-Origin": "*",  # Allow from any origin
+        "Access-Control-Allow-Methods": "GET, OPTIONS",  # Allow specific methods
+        "Access-Control-Allow-Headers": "Content-Type, Authorization"  # Allow specific headers
+    }
+    
+def post_auth_header():
+    return  {
+        "Access-Control-Allow-Origin": "*",  # Allow from any origin
+        "Access-Control-Allow-Methods": "POST, OPTIONS",  # Allow specific methods
+        "Access-Control-Allow-Headers": "Content-Type, Authorization"  # Allow specific headers
+    }
+
+def put_auth_header():
+    return  {
+        "Access-Control-Allow-Origin": "*",  # Allow from any origin
+        "Access-Control-Allow-Methods": "PUT, OPTIONS",  # Allow specific methods
+        "Access-Control-Allow-Headers": "Content-Type, Authorization"  # Allow specific headers
+    }
+
+def delete_auth_header():
+    return  {
+        "Access-Control-Allow-Origin": "*",  # Allow from any origin
+        "Access-Control-Allow-Methods": "DELETE, OPTIONS",  # Allow specific methods
+        "Access-Control-Allow-Headers": "Content-Type, Authorization"  # Allow specific headers
+    }

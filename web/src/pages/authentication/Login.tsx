4a118cc0683f3a63e0379b56f30dff33e7f6262e
ConnerDeFeo <ninjanerozz@gmail.com>
2025-10-10 19:22:45 -0400

    home page done"

--- web/src/pages/authentication/Login.tsx
+++ web/src/pages/authentication/Login.tsx
@@ -8,13 +8,13 @@ const Login = () => {
     const [password, setPassword] = useState('');
     const [error, setError] = useState<string | null>(null);
     const [_, setAccountType] = useContext(AuthContext);
-
     const handleSubmit = async () => {
         try {
             await UserService.signIn(email, password);
             const accountType: AccountType = await UserService.getAccountType();
             if (accountType !== AccountType.SignedOut) {
                 setAccountType(accountType);
+                window.location.replace('/'); // Redirect to home page after successful login
             } 
         } catch (error) {
             setError(error instanceof Error ? error.message : 'An unknown error occurred.');
